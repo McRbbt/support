e76297f74b09310ce8d6cf0d69c91832a97964c1
McRbbt <99355278+McRbbt@users.noreply.github.com>
2022-06-06 05:30:27 +0200

    Update checkwindows.bat

--- checkwindows.bat
+++ checkwindows.bat
@@ -1,5 +1,2 @@
 @echo on
-sfc /scannow && dism /online /cleanup-image /scanhealth && dism /online /cleanup-image /checkhealth && dism /online /cleanup-image /restorehealth
-
-
-timeout /t 50
+sfc /scannow && dism /online /cleanup-image /scanhealth && dism /online /cleanup-image /checkhealth && dism /online /cleanup-image /restorehealth && cleanmgr.exe /VERYLOWDISK
